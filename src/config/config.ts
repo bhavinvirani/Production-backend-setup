b32e1a8222dd0a7c98f6276f12cbaf1b2e706ab6
bhavinvirani <bhavinvirani45@gmail.com>
2025-02-13 00:13:14 -0500

    feat: Environment setup

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+import DotenvFlow from 'dotenv-flow'
+
+DotenvFlow.config()
+
+export default {
+    ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT || 3000,
+    SERVER_URL: process.env.SERVER_URL,
+
+    // Database
+    DATABASE_URL: process.env.DATABASE_URL,
+    
+}
